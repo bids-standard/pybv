74690e2e7bbb21d9414f93c782398e19e96f93a7
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-04-28 13:18:32 +0200

    release 0.0.2
    * minor fixes, adding docs on how to release, bumping version
    
    * clarify release workflow
    
    * rebasing fix

--- pybv/__init__.py
+++ pybv/__init__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-"""pybv: a lightweight BrainVision I/O tool"""
+"""pybv: a lightweight BrainVision I/O tool."""
 
 # Authors: Philip Alday <phillip.alday@unisa.edu.au>
 #          Chris Holdgraf <choldgraf@berkeley.edu>
 #          Stefan Appelhoff <stefan.appelhoff@mailbox.org>
+#          Tristan Stenner <stenner@med-psych.uni-kiel.de>
 #
 # License: BSD (3-clause)
 
-__version__ = '0.0.1-dev'
+__version__ = '0.0.2'
 
 from .io import (write_brainvision, )  # noqa: F401
