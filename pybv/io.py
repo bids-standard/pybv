9584adbb6951730f0e4e235944d0a11ed94bc966
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2019-07-18 10:50:18 +0200

    padding in event markers is commonly at least 3

--- pybv/io.py
+++ pybv/io.py
@@ -156,8 +156,12 @@ def _write_vmrk_file(vmrk_fname, eeg_fname, events):
         if events is None or len(events) == 0:
             return
 
-        # Handle events
+        # Handle events: We write all of them as "Stimulus" events for now.
+        # This is a string staring with "S" and followed by an integer of
+        # minimum length 3, padded with "space" if the integer is < length 3.
+        # For example "S  1", "S 23", "S345"
         twidth = int(np.ceil(np.log10(np.max(events[:, 1]))))
+        twidth = twidth if twidth > 3 else 3
         tformat = 'S{:>' + str(twidth) + '}'
 
         for ii, irow in enumerate(range(len(events)), start=2):
