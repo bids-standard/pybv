50e5ff11f593a3dda4293294c8311ff728d2f577
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2022-06-04 15:36:45 +0200

    REL: 0.7.3

--- pybv/__init__.py
+++ pybv/__init__.py
@@ -1,7 +1,7 @@
 """A lightweight I/O utility for the BrainVision data format."""
 
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 from .io import write_brainvision
 
 __all__ = ['write_brainvision']
