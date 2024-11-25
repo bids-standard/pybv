fd6afff23248c7d5d6cc967f4e954c7a92b604d2
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2024-11-25 21:22:33 +0100

    fix circular import

--- pybv/__init__.py
+++ pybv/__init__.py
@@ -3,13 +3,13 @@
 # Authors: pybv developers
 # SPDX-License-Identifier: BSD-3-Clause
 
-from .io import write_brainvision
-
-__all__ = ["write_brainvision"]
-
 try:
     from importlib.metadata import version
 
     __version__ = version("pybv")
 except Exception:
     __version__ = "0.0.0"
+
+from pybv.io import write_brainvision
+
+__all__ = ["write_brainvision"]
